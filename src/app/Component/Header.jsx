d2dc6c125ebad63471f5943bbb715eb9259edade
Kabita Rokaya <kabitarokaya9@gmail.com>
2025-05-26 12:34:40 +0545

    Update Header.jsx

--- src/app/Component/Header.jsx
+++ src/app/Component/Header.jsx
@@ -15,6 +15,11 @@ import { GoSearch } from "react-icons/go";
 import { FaHeart } from "react-icons/fa";
 import { useCart } from './CartContext';
 // import CartContext from "./CartContext";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { IoBagCheckSharp } from "react-icons/io5";
+import { CiSettings } from "react-icons/ci";
+
 function Header() {
       const { state } = useCart();
 
@@ -99,10 +104,11 @@ function Header() {
                                                     <p className='font-bold text-blue-700 cursor-pointer'>Sign Up</p>
                                                 </div>
                                                 <ul className="p-2">
-                                                    <li className='flex gap-2 py-1 cursor-pointer'>My Profile</li>
-                                                    <li className='flex gap-2 py-1 cursor-pointer'>Orders</li>
+                                                    <li className='flex gap-2 py-1 cursor-pointer'> <FaRegUserCircle className='size-[20px]' />My Profile</li>
+                                                    <li className='flex gap-2 py-1 cursor-pointer'> <FaBook  className='size-[20px]'/>My Address Book</li>
                                                     <li className='flex gap-2 py-1 cursor-pointer'>Wishlist</li>
-                                                    <li className='flex gap-2 py-1 cursor-pointer'>Settings</li>
+                                                  <li className='flex gap-2 py-1 cursor-pointer'><IoBagCheckSharp className='size-[20px]'/>My Order</li>
+                                                    <li className='flex gap-2 py-1 cursor-pointer'><CiSettings className='size-[20px]'/>Settings</li>
                                                 </ul>
                                             </div>
                                         )}
